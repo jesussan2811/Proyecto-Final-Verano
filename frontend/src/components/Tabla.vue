d34cc34c5f11d68d85fbce2d41ef912728800040
MiguelMariscal <mariscalmiguel9@gmail.com>
2021-07-28 22:48:47 -0600

    install axios and vue-axios

---
+++ frontend/src/components/Tabla.vue
@@ -0,0 +1,30 @@
+<template>
+  <b-table striped hover :items="items" :fields="fields" :busy="busy">
+
+    <template #table-busy>
+      <div class="text-center text-danger my-2">
+        <b-spinner class="align-middle"></b-spinner>
+        <strong>Cargando...</strong>
+      </div>
+    </template>
+
+    <template #cell(acciones)="data">
+      <slot name="actions" :item="data.item"></slot>
+    </template>
+
+  </b-table>
+</template>
+
+<script>
+export default {
+  name: "Tabla",
+  props: {
+    items: Array,
+    fields: Array,
+    busy: {
+        type: Boolean,
+        default: false
+    }
+  },
+};
+</script>
